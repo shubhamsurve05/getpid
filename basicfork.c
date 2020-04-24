fddad3c86afb55b944fbe0d8ac952854834359c9
shubham surve <shubhamsurve60@gmail.com>
2020-04-24 17:13:33 +0530

    shubham

--- basicfork.c
+++ basicfork.c
@@ -1,11 +1,9 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-int main()
-{
-  fork();
-  fork(); 
-  printf("called fork() system call\n");
-  return 0; 
+int main() {
+   fork();
+   printf("Called fork() system call\n");
+   return 0;
 }
